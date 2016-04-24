1c461a475db6c3f62f69dd977c8de254b0d67638
s4w3d0ff <s4w3d0ff@sawedoff.net>
2016-04-23 20:46:11 -0700

    Python 3 encoding fix
    and minor changes...
    - Moved the date variables into the Poloniex class, just seemed like a better fit and easier to access.

--- poloniex.py
+++ poloniex.py
@@ -12,13 +12,6 @@ else:
 	from urllib2 import Request, urlopen
 	from urllib import urlencode
 
-minute = 60
-hour = minute*60
-day = hour*24
-week = day*7
-month = day*30
-year = day*365
-
 # Possible Commands
 PUBLIC_COMMANDS = ['returnTicker', 'return24hVolume', 'returnOrderBook', 'returnTradeHistory', 'returnChartData', 'returnCurrencies', 'returnLoanOrders'] 
 PRIVATE_COMMANDS = ['returnBalances', 'returnCompleteBalances', 'returnDepositAddresses', 'generateNewAddress', 'returnDepositsWithdrawals', 'returnOpenOrders', 'returnTradeHistory', 'returnAvailableAccountBalances', 'returnTradableBalances', 'returnOpenLoanOffers', 'returnActiveLoans', 'createLoanOffer', 'cancelLoanOffer', 'toggleAutoRenew', 'buy', 'sell', 'cancelOrder', 'moveOrder', 'withdraw', 'transferBalance', 'returnMarginAccountSummary', 'marginBuy', 'marginSell', 'getMarginPosition', 'closeMarginPosition']
@@ -26,7 +19,15 @@ PRIVATE_COMMANDS = ['returnBalances', 'returnCompleteBalances', 'returnDepositAd
 class Poloniex:
 	def __init__(self, APIKey='', Secret=''):
 		self.APIKey = APIKey
-		self.Secret = Secret
+		self.Secret = Secret.encode('utf8')
+		
+		self.MINUTE = 60
+		self.HOUR = self.MINUTE*60
+		self.DAY = self.HOUR*24
+		self.WEEK = self.DAY*7
+		self.MONTH = self.DAY*30
+		self.YEAR = self.DAY*365
+		
 		# Conversions
 		self.timestamp_str = lambda timestamp=time.time(), format="%Y-%m-%d %H:%M:%S": datetime.fromtimestamp(timestamp).strftime(format)
 		self.str_timestamp = lambda datestr=self.timestamp_str(), format="%Y-%m-%d %H:%M:%S": int(time.mktime(time.strptime(datestr, format)))
@@ -37,12 +38,12 @@ class Poloniex:
 		self.marketVolume = lambda x=0: self.api('return24hVolume')
 		self.marketStatus = lambda x=0: self.api('returnCurrencies')
 		self.marketLoans = lambda coin: self.api('returnLoanOrders',{'currency':coin})
-		self.marketOrders = lambda pair='all': self.api('returnOrderBook', {'currencyPair':pair})
-		self.marketChart = lambda pair, period=day, start=time.time()-(month*2), end=time.time(): self.api('returnChartData', {'currencyPair':pair, 'period':period, 'start':start, 'end':end})
-		self.marketTradeHist = lambda pair: self.api('returnTradeHistory',{'currencyPair':pair}) # NEEDS TO BE FIXED ON Poloniex
+		self.marketOrders = lambda pair='all', depth=50: self.api('returnOrderBook', {'currencyPair':pair, 'depth':depth})
+		self.marketChart = lambda pair, period=self.DAY, start=0000000, end=time.time(): self.api('returnChartData', {'currencyPair':pair, 'period':period, 'start':start, 'end':end})
+		#self.marketTradeHist = lambda pair: self.api('returnTradeHistory',{'currencyPair':pair})# NEEDS TO BE FIXED ON Poloniex
 		
 		#PRIVATE COMMANDS
-		#self.myTradeHist = lambda pair: self.api('returnTradeHistory',{'currencyPair':pair})
+		self.myTradeHist = lambda pair: self.api('returnTradeHistory',{'currencyPair':pair})
 		self.myAvailBalances = lambda x=0: self.api('returnAvailableAccountBalances')
 		self.myMarginAccountSummary = lambda x=0: self.api('returnMarginAccountSummary')
 		self.myMarginPosition = lambda pair='all': self.api('getMarginPosition',{'currencyPair':pair})
@@ -58,8 +59,8 @@ class Poloniex:
 		self.cancelLoanOrder = lambda orderId: self.api('cancelLoanOffer', {'orderNumber':orderId})
 		self.toggleAutoRenew = lambda orderId: self.api('toggleAutoRenew', {'orderNumber':orderId})
 		self.closeMarginPosition = lambda pair: self.api('closeMarginPosition',{'currencyPair':pair})
-		self.marginBuy = lambda pair, rate, amount, lendingRate=2: self.api('marginBuy',{'currencyPair':pair, 'rate':rate, 'amount':amount, 'lendingRate':lendingRate})
-		self.marginSell= lambda pair, rate, amount, lendingRate=2: self.api('marginSell',{'currencyPair':pair, 'rate':rate, 'amount':amount, 'lendingRate':lendingRate})
+		self.marginBuy = lambda pair, rate, amount, lendingRate=2: self.api('marginBuy', {'currencyPair':pair, 'rate':rate, 'amount':amount, 'lendingRate':lendingRate})
+		self.marginSell= lambda pair, rate, amount, lendingRate=2: self.api('marginSell', {'currencyPair':pair, 'rate':rate, 'amount':amount, 'lendingRate':lendingRate})
 		self.buy = lambda pair, rate, amount: self.api('buy', {'currencyPair':pair, 'rate':rate, 'amount':amount})
 		self.sell = lambda pair, rate, amount: self.api('sell', {'currencyPair':pair, 'rate':rate, 'amount':amount})
 		self.cancelOrder = lambda orderId: self.api('cancelOrder', {'orderNumber':orderId})
@@ -81,11 +82,12 @@ class Poloniex:
 				print("An APIKey and Secret is needed!")
 				return False
 			url, args['nonce'] = ['https://poloniex.com/tradingApi', int(time.time()*42)]
-			post_data = urlencode(args)
+			post_data = urlencode(args).encode('utf8')
 			sign = hmac.new(self.Secret, post_data, hashlib.sha512).hexdigest()
 			headers = {'Sign': sign, 'Key': self.APIKey}
 			ret = urlopen(Request(url, post_data, headers))
 			return json.loads(ret.read().decode(encoding='UTF-8'))
+		
 		elif command in PUBLIC_COMMANDS:
 			url = 'https://poloniex.com/public?'
 			if not args:
