65ca8211a68fadac92d20b3e64a664b045995ad8
s4w3d0ff <s4w3d0ff@sawedoff.net>
2016-04-25 17:45:07 -0700

    data -> params
    This still gave a `{u'error': u'Invalid command.'}` from the api server but the `r.url` looks like it probably should be. 
    `https://poloniex.com/tradingApi?nonce=61388502640&command=returnOpenOrders&currencyPair=all`

--- poloniex.py
+++ poloniex.py
@@ -86,7 +86,7 @@ class Poloniex:
 			post_data = urlencode(args).encode('utf8')
 			sign = hmac.new(self.Secret, post_data, hashlib.sha512).hexdigest()
 			headers = {'Key': self.APIKey, 'Sign': sign}
-			r = requests.post(url, data=post_data, headers=headers, timeout=27)
+			r = requests.post(url, params=post_data, headers=headers, timeout=27)
 			#ret = urlopen(Request(url, post_data, headers))
 			#return json.loads(ret.read().decode(encoding='UTF-8'))
 			print(r.url)
