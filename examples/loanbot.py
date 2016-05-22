7a72e7f276192faab2243b810ef79e002cd13e80
s4w3d0ff <s4w3d0ff@sawedoff.net>
2016-05-22 08:14:01 -0700

    Basic module for a loan bot
    Loans out EVERYTHING in your 'lending' account and checks if old loan offers have become 'stale', then cancels them

---
+++ examples/loanbot.py
@@ -0,0 +1,71 @@
+import time
+from multiprocessing.dummy import Process as Thread
+from poloniex import Poloniex
+
+class Loaner():
+	""" Object for control of threaded Loaner loop"""
+	def __init__(self, Key, Secret, interval=60*2, ageLimit=60*5, offset=2):
+		"""
+		- <Key> - Polo Api key
+		- <Secret> - Polo Api secret
+		- Loaner.RUNNING = bool (set to 'False' to stop main thread)
+		- Loaner.INTERVAL = time in sec to wait between updates [default= 2min]
+		- Loaner.AGELIMIT = max age (in sec) for an open loan offer [default= 5min]
+		- Loaner.OFFSET = offset from the top loan offer rate (offset*0.000001) [default= 2]
+		- Loaner.CHECKINT = number of times to check Loaner.RUNNING between intervals (could be hard on cpu if set too high and INTERVAL set too low!) [default= 20]
+		- Loaner.MINAMOUNT = Minimum amount for creating loan offers [default= 0.01]
+		"""
+		self.POLO = Poloniex(Key, Secret)
+		self.INTERVAL, self.AGELIMIT, self.OFFSET, self.CHECKINT, self.MINAMOUNT, self.RUNNING = [interval, ageLimit, offset, 20, 0.01, False]
+		self.thread = Thread(target=self.run);self.thread.daemon = True
+	
+	def run(self):
+		""" Main loop that is threaded (set Loaner.RUNNING to 'False' to stop loop)"""
+		while self.RUNNING:
+			try:
+				self.cancelOldLoans(self.POLO.myOpenLoanOrders(), self.AGELIMIT)
+				self.createLoans(self.POLO.myAvailBalances(), self.OFFSET)
+				for i in range(self.CHECKINT):
+					if not self.RUNNING: break
+					time.sleep(self.INTERVAL/self.CHECKINT)
+			except Exception as e: 
+			# Sometimes (when losing connection) the loop will 'stall' from urlopen (in poloniex) and I have not figured out a way to capture it yet....
+			# maybe a 'timeout' setting in the revive socket will help...
+				print(e);time.sleep(self.INTERVAL/self.CHECKINT)
+	
+	def start(self):
+		""" Start Loaner.thread"""
+		self.RUNNING = True;self.thread.start()
+		print('LOANER: started')
+	
+	def stop(self):
+		""" Stop Loaner.thread"""
+		self.RUNNING = False;self.thread.join()
+		print('LOANER: stopped')
+	
+	def cancelOldLoans(self, orderList, ageLimit):
+		""" Cancel loans in <orderList> that are older than <ageLimit>
+			
+			- orderList = JSON object received from poloniex (open loan orders)
+			- ageLimit = max age to allow an order to sit still before canceling (in seconds)""" 
+		print('LOANER: Canceling stale loans')
+		for market in orderList:
+			for order in orderList[market]:
+				print('LOANER: %s order %s has been open %f mins' % (market, str(order['id']), round((time.time()-self.POLO.UTCstr2epoch(order['date']))/60, 2)))
+				if time.time()-self.POLO.UTCstr2epoch(order['date']) > ageLimit:
+					result = self.POLO.cancelLoanOrder(int(order['id']))
+					if not 'error' in result: print('LOANER: %s %s [%s]' % (market, result["message"].lower(), str(order['id'])))
+					else: print('LOANER: %s' % result['error'])
+	
+	def createLoans(self, balances, offset):
+		""" Create loans for all markets in <balances> at the <offset> from the top rate
+			
+			- balances = JSON object received from poloniex (available balances)
+			- offset = number of 'loanToshis' to offset from the top loan order (offset*0.000001)""" 
+		if 'lending' in balances:
+			print('LOANER: Creating loans')
+			for market in balances['lending']:
+				if float(balances['lending'][market]) > self.MINAMOUNT:
+					result = self.POLO.createLoanOrder(market, float(balances['lending'][market]), float(self.POLO.marketLoans(market)['offers'][0]['rate'])+(offset*0.000001))
+					if not 'error' in result: print('LOANER: %f %s %s' % (float(balances['lending'][market]), market, result["message"].lower()))
+					else: print('LOANER: %s' % result['error'])
