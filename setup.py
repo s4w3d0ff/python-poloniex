7567862d18579cb14e652d60bcd554edb1a84dad
s4w3d0ff <s4w3d0ff@sawedoff.net>
2016-06-03 13:32:20 -0700

    Python 3 fix
    hmac and hashlib are part of standard lib for Python 3 but not Python 2

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
 from setuptools import setup
-
-setup(name='poloniex',
-      version='0.1',
-      description='Poloniex API wrapper for Python 2.7 and 3',
-      url='https://github.com/s4w3d0ff/python-poloniex',
-      author='s4w3d0ff',
-      license='GPL v2',
-      packages=['poloniex'],
-      install_requires=['hmac','hashlib'],
-      zip_safe=False)
-
+import sys
+if sys.version_info[0] == 3:
+	setup(name='poloniex',
+		version='0.1',
+		description='Poloniex API wrapper for Python 2.7 and 3',
+		url='https://github.com/s4w3d0ff/python-poloniex',
+		author='s4w3d0ff',
+		license='GPL v2',
+		packages=['poloniex'],
+		zip_safe=False)
+else:
+	setup(name='poloniex',
+		version='0.1',
+		description='Poloniex API wrapper for Python 2.7 and 3',
+		url='https://github.com/s4w3d0ff/python-poloniex',
+		author='s4w3d0ff',
+		license='GPL v2',
+		packages=['poloniex'],
+		install_requires=['hmac','hashlib'],
+		zip_safe=False)
