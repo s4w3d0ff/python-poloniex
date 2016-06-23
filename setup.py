98a76f8e5c9ce1d98411dec79c6fa53e41021608
chenhouwu <chenhouwu@gmail.com>
2016-06-24 03:18:11 +0800

    no need to distinguish python versions
    python 2.7 already shipped with hmac and hashlib

--- setup.py
+++ setup.py
@@ -1,22 +1,12 @@
 from setuptools import setup
 import sys
-if sys.version_info[0] == 3:
-	setup(name='poloniex',
-		version='0.1',
-		description='Poloniex API wrapper for Python 2.7 and 3',
-		url='https://github.com/s4w3d0ff/python-poloniex',
-		author='s4w3d0ff',
-		license='GPL v2',
-		packages=['poloniex'],
-		install_requires=['requests'],
-		zip_safe=False)
-else:
-	setup(name='poloniex',
-		version='0.1',
-		description='Poloniex API wrapper for Python 2.7 and 3',
-		url='https://github.com/s4w3d0ff/python-poloniex',
-		author='s4w3d0ff',
-		license='GPL v2',
-		packages=['poloniex'],
-		install_requires=['hmac','hashlib','requests'],
-		zip_safe=False)
+
+setup(name='poloniex',
+	version='0.1',
+	description='Poloniex API wrapper for Python 2.7 and 3',
+	url='https://github.com/s4w3d0ff/python-poloniex',
+	author='s4w3d0ff',
+	license='GPL v2',
+	packages=['poloniex'],
+	install_requires=['requests'],
+	zip_safe=False)
