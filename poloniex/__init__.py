b2d9f2009bbdcd926e236e9710b704d12022a0b5
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2016-09-09 12:42:10 -0700

    GPL header

--- poloniex/__init__.py
+++ poloniex/__init__.py
@@ -1,5 +1,4 @@
 # Poloniex API wrapper tested on Python 2.7.6 & 3.4.3
-# Coding style complies with PEP8 standards
 # https://github.com/s4w3d0ff/python-poloniex
 # BTC: 15D8VaZco22GTLVrFMAehXyif6EGf8GMYV
 # TODO:
@@ -8,13 +7,26 @@
 #   [ ] Find out if request module has the equivalent to urlencode
 #   [ ] Add Push Api application wrapper
 #   [ ] Convert docstrings to sphinx
-
-# standard
+#
+#    Copyright (C) 2016  https://github.com/s4w3d0ff
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 import sys
 import time
 import calendar
 import logging
-# built-in
 import json
 import hmac
 import hashlib
