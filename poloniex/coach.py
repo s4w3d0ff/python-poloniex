19b1e42384b273ef2a75e19fc29740f03fd40a5b
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2016-09-10 22:25:30 -0700

    Move time convertions

--- poloniex/coach.py
+++ poloniex/coach.py
@@ -1,4 +1,22 @@
-from time import sleep, time
+#    coach.py
+#    Copyright (C) 2016  https://github.com/s4w3d0ff
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+from time import sleep, time, gmtime, strftime, strptime, localtime, mktime
+from calendar import timegm
 
 class Coach(object):
     """
@@ -10,8 +28,48 @@ class Coach(object):
         timeFrame = float time in secs [default = 1.0]
         callLimit = int max amount of calls per 'timeFrame' [default = 6]
         """
-        self._timeFrame, self._callLimit = [timeFrame, callLimit]
+        self._timeFrame, self._callLimit = timeFrame, callLimit
         self._timeBook = []
+
+    # Convertions
+    def epoch2UTCstr(self, timestamp=time(), fmat="%Y-%m-%d %H:%M:%S"):
+        """
+        - takes epoch timestamp
+        - returns UTC formated string
+        """
+        return strftime(fmat, gmtime(timestamp))
+
+    def UTCstr2epoch(self, datestr=False, fmat="%Y-%m-%d %H:%M:%S"):
+        """
+        - takes UTC date string
+        - returns epoch
+        """
+        if not datestr:
+            datestr = self.epoch2UTCstr()
+        return timegm(strptime(datestr, fmat))
+
+    def epoch2localstr(self, timestamp=time(), fmat="%Y-%m-%d %H:%M:%S"):
+        """
+        - takes epoch timestamp
+        - returns localtimezone formated string
+        """
+        return strftime(fmat, localtime(timestamp))
+
+    def localstr2epoch(self, datestr=False, fmat="%Y-%m-%d %H:%M:%S"):
+        """
+        - takes localtimezone date string,
+        - returns epoch
+        """
+        if not datestr:
+            datestr = self.epoch2UTCstr()
+        return mktime(strptime(datestr, fmat))
+
+    def float2roundPercent(self, floatN, decimalP=2):
+        """
+        - takes float
+        - returns percent(*100) rounded to the Nth decimal place as a string
+        """
+        return str(round(float(floatN)*100, decimalP))+"%"
 
     def wait(self):
         """ Makes sure our api calls don't go past the api call limit """
