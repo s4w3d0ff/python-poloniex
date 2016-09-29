0f6d2e4005963e16c8d3fdb7727ca17e61647727
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2016-09-29 14:52:59 -0700

    returnLendingHistory command
    new private api command

--- poloniex/__init__.py
+++ poloniex/__init__.py
@@ -61,6 +61,7 @@ PRIVATE_COMMANDS = [
     'returnOpenLoanOffers',
     'returnOrderTrades',
     'returnActiveLoans',
+    'returnLendingHistory',
     'createLoanOffer',
     'cancelLoanOffer',
     'toggleAutoRenew',
