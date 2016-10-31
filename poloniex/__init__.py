13df689b806c953479091b42328027dfebd32a3f
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2016-10-31 15:25:12 -0700

    fix nesting

--- poloniex/__init__.py
+++ poloniex/__init__.py
@@ -199,15 +199,13 @@ class Poloniex(object):
                             'Key': self.Key
                             },
                         timeout=self.timeout)
-                # return decoded json
-                try:
-                    return _loads(ret.text, parse_float=unicode)
-                except NameError:
-                    return _loads(ret.text, parse_float=str)
-                
             except Exception as e:
                 raise e
-
+            # return decoded json
+            try:
+                return _loads(ret.text, parse_float=unicode)
+            except NameError:
+                return _loads(ret.text, parse_float=str)
             finally:
                 # increment nonce(no matter what)
                 self.nonce += 1
@@ -218,12 +216,12 @@ class Poloniex(object):
                 ret = _post(
                         'https://poloniex.com/public?' + _urlencode(args),
                         timeout=self.timeout)
-                try:
-                    return _loads(ret.text, parse_float=unicode)
-                except NameError:
-                    return _loads(ret.text, parse_float=str)
             except Exception as e:
                 raise e
+            try:
+                return _loads(ret.text, parse_float=unicode)
+            except NameError:
+                return _loads(ret.text, parse_float=str)
         else:
             raise ValueError("Invalid Command!")
 
@@ -291,12 +289,12 @@ class Poloniex(object):
                         'end': str(end)
                         }),
                     timeout=self.timeout)
-            try:
-                return _loads(ret.text, parse_float=unicode)
-            except NameError:
-                return _loads(ret.text, parse_float=str)
         except Exception as e:
             raise e
+        try:
+            return _loads(ret.text, parse_float=unicode)
+        except NameError:
+            return _loads(ret.text, parse_float=str)
 
     # --PRIVATE COMMANDS------------------------------------------------------
     def returnTradeHistory(self, pair):
