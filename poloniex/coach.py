3435bf30791005e561493bd01f9ec203a4b36817
Josh Klar <j@iv597.com>
2016-11-05 15:31:44 -0500

    Unbreak a NameError when using Poloniex(coach=True)

--- poloniex/coach.py
+++ poloniex/coach.py
@@ -15,6 +15,7 @@
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
+import logging
 from time import sleep, time, gmtime, strftime, strptime, localtime, mktime
 from calendar import timegm
 
