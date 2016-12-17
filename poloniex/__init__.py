f93841c37a94887a6be2d612c51c1cfe866f5146
Josh Klar <j@iv597.com>
2016-12-16 19:57:57 -0600

    Resolves s4w3d0ff/python-poloniex#38, adding advanced buy order flags

--- poloniex/__init__.py
+++ poloniex/__init__.py
@@ -411,21 +411,65 @@ class Poloniex(object):
             'lendingRate': str(lendingRate)
         })
 
-    def buy(self, pair, rate, amount):
+    def buy(self, pair, rate, amount, fill_or_kill=False, immediate_or_cancel=False, post_only=False):
         """ Creates buy order for <pair> at <rate> for <amount> """
+<<<<<<< HEAD
         return self.__call__('buy', {
             'currencyPair': str(pair).upper(),
             'rate': str(rate),
             'amount': str(amount)
         })
+=======
+        excl_args = [x for x in (fill_or_kill, immediate_or_cancel, post_only) if x]
 
-    def sell(self, pair, rate, amount):
+        if len(excl_args) > 1:
+            raise ValueError('fill_or_kill, immediate_or_cancel, post_only are mutually exclusive')
+
+        req = {
+            'currencyPair': str(pair).upper(),
+            'rate': str(rate),
+            'amount': str(amount),
+        }
+
+        if fill_or_kill:
+            req['fillOrKill'] = 1
+        elif immediate_or_cancel:
+            req['immediateOrCancel'] = 1
+        elif post_only:
+            req['postOnly'] = 1
+
+        return self.__call__('buy', req)
+>>>>>>> 7f7bd75... Resolves s4w3d0ff/python-poloniex#38, adding advanced buy order flags
+
+    def sell(self, pair, rate, amount, fill_or_kill=False, immediate_or_cancel=False, post_only=False):
         """ Creates sell order for <pair> at <rate> for <amount> """
+<<<<<<< HEAD
         return self.__call__('sell', {
             'currencyPair': str(pair).upper(),
             'rate': str(rate),
             'amount': str(amount)
         })
+=======
+        excl_args = [x for x in (fill_or_kill, immediate_or_cancel, post_only) if x]
+
+        if len(excl_args) > 1:
+            raise ValueError('fill_or_kill, immediate_or_cancel, post_only are mutually exclusive')
+
+        req = {
+            'currencyPair': str(pair).upper(),
+            'rate': str(rate),
+            'amount': str(amount),
+        }
+
+        if fill_or_kill:
+            req['fillOrKill'] = 1
+        elif immediate_or_cancel:
+            req['immediateOrCancel'] = 1
+        elif post_only:
+            req['postOnly'] = 1
+
+        return self.__call__('sell', req)
+>>>>>>> 7f7bd75... Resolves s4w3d0ff/python-poloniex#38, adding advanced buy order flags
 
     def cancelOrder(self, orderId):
         """ Cancels order <orderId> """
