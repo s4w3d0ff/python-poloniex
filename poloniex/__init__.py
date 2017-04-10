d174249292a9149d3a49dba1858d7a9c515b88b7
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2017-04-10 12:38:16 -0700

    Update __init__.py

--- poloniex/__init__.py
+++ poloniex/__init__.py
@@ -23,11 +23,16 @@
 #    You should have received a copy of the GNU General Public License along
 #    with this program; if not, write to the Free Software Foundation, Inc.,
 #    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-# python 3 voodoo
+# python 2
 try:
-    from urllib.parse import urlencode as _urlencode
-except:
     from urllib import urlencode as _urlencode
+    basestring = basestring
+    unicode = unicode
+# python 3
+except:
+    from urllib.parse import urlencode as _urlencode
+    basestring = str
+    unicode = str
 from json import loads as _loads
 from hmac import new as _new
 from hashlib import sha512 as _sha512
@@ -174,10 +179,7 @@ class Poloniex(object):
                 raise e
             # return decoded json
             if not self.jsonNums:
-                try:
-                    return _loads(ret.text, parse_float=unicode)
-                except NameError:
-                    return _loads(ret.text, parse_float=str)
+                return _loads(ret.text, parse_float=str)
             return _loads(ret.text, parse_float=self.jsonNums, parse_int=self.jsonNums)
 
         # public?
@@ -190,10 +192,7 @@ class Poloniex(object):
             except Exception as e:
                 raise e
             if not self.jsonNums:
-                try:
-                    return _loads(ret.text, parse_float=unicode)
-                except NameError:
-                    return _loads(ret.text, parse_float=str)
+                return _loads(ret.text, parse_float=str)
             return _loads(ret.text, parse_float=self.jsonNums, parse_int=self.jsonNums)
         else:
             raise ValueError("Invalid Command!")
@@ -266,10 +265,7 @@ class Poloniex(object):
         except Exception as e:
             raise e
         if not self.jsonNums:
-            try:
-                return _loads(ret.text, parse_float=unicode)
-            except NameError:
-                return _loads(ret.text, parse_float=str)
+            return _loads(ret.text, parse_float=str)
         return _loads(ret.text, parse_float=self.jsonNums, parse_int=self.jsonNums)
 
     # --PRIVATE COMMANDS------------------------------------------------------
