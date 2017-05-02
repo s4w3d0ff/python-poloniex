4f696b8d310a965fea090dc5e7df2d7262213843
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2017-05-02 14:50:29 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name='poloniex',
-      version='0.4.1',
+      version='0.4.2',
       description='Poloniex API wrapper for Python 2.7 and 3',
       url='https://github.com/s4w3d0ff/python-poloniex',
       author='s4w3d0ff',
