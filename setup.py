1f27ff2f5b9b2150d5ad6e5105b30d905a7606b0
s4w3d0ff <s4w3d0ff@users.noreply.github.com>
2019-05-28 22:04:31 -0700

    add long desc

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
 from setuptools import setup
+
+read_md = lambda f: open(f, 'r').read()
+
 setup(name='poloniexapi',
       version='0.5.0',
       description='Poloniex API wrapper for Python 2.7 and 3',
+      long_description=read_md('README.md'),
+      long_description_content_type='text/markdown',
       url='https://github.com/s4w3d0ff/python-poloniex',
       author='s4w3d0ff',
       author_email="info@s4w3d0ff.host",
